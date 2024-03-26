b5de17ea818ef0f5fcb6ac4e84211e49a497ac6f
= <jake44264@hotmail.co.uk>
2024-03-26 16:40:58 +0000

    feat: updating database store location for kubernetes volume

--- cmd/master/initializers/db.go
+++ cmd/master/initializers/db.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func InitializeDB() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("load_testing.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("db/load_testing.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
